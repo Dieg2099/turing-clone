08a2daa8fc8be6285318ee72bd6e79e97e773866
Dieg2099 <diego.martinez@cimav.edu.mx>
2025-05-31 01:31:58 -0600

    test4

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App() {
+  return (
+    <div>Hola Mundo</div>
+  );
+}
